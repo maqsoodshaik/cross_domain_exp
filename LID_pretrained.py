0727cc50d881602c24bcbd5d308c603bfb5c7478
Mohammed Maqsood shaik <shaikmdmaqsood@gmail.com>
2022-10-06 01:37:39 +0200

    corrected preteain and updated wop

--- LID_pretrained.py
+++ LID_pretrained.py
@@ -173,9 +173,9 @@ print(trainer.train())
 print(trainer.evaluate())
 
 """You can now upload the result of the training to the Hub, just execute this instruction:"""
-trainer.save_model( f"/pretrained/{model_name}_{dataset}_bestmodel")
+trainer.save_model( f"/pretrained/{model_name}_bestmodel")
 best_model = AutoModelForAudioClassification.from_pretrained(
-    f"/pretrained/{model_name}_{dataset}_bestmodel"
+    f"/pretrained/{model_name}_bestmodel"
 )
 trainer = Trainer(
     best_model,
--- submit_wop.sh
+++ submit_wop.sh
@@ -9,6 +9,6 @@ nvidia-smi
 
 # DIR=$(cd $(dirname $0); pwd)
 
-export HF_DATASETS_DOWNLOADED_DATASETS_PATH='/corpora/common_voice_speech/'
+export HF_DATASETS_DOWNLOADED_DATASETS_PATH='/corpora/multilingual_librispeech/'
 export HF_DATASETS_CACHE='/cache'
 python -u ~/thesis/cross_domain_exp/LID_wop.py 
