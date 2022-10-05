3858473fc044f2ec5980c0da29cd57e18f819a95
maqsoodshaik <shaikmdmaqsood@gmail.com>
2022-10-06 01:13:13 +0200

    plotting and checking fleurs model

--- LID_eval_pretrained.py
+++ LID_eval_pretrained.py
@@ -4,18 +4,18 @@ model_checkpoint = "facebook/wav2vec2-base"
 batch_size = 32
 from os import rename
 from datasets import load_dataset, load_metric,concatenate_datasets,Dataset
-
-configs = ['uk','ab','pl','ru']
+dataset_name = "fleurs"
+configs = ['fr_fr','de_de','nl_nl']
 list_datasets_validation = []
 for i in configs:   
-    dataset_validation = load_dataset("common_voice",i,split = "validation")
+    dataset_validation = load_dataset("google/fleurs",i,split = "validation")
     dataset_validation = Dataset.from_dict(dataset_validation[:20])
     list_datasets_validation.append(dataset_validation)
 dataset_validation = concatenate_datasets(
         list_datasets_validation
     )
 metric = load_metric("accuracy")
-labels = configs
+labels =["French","German","Dutch"]
 label2id, id2label,label2id_int = dict(), dict(),dict()
 for i, label in enumerate(labels):
     label2id[label] = str(i)
@@ -36,13 +36,13 @@ def preprocess_function(examples):
         max_length=int(feature_extractor.sampling_rate * max_duration), 
         truncation=True, 
     )
-    inputs["label"] = [label2id_int[image] for image in examples["locale"]]
+    inputs["labels"] = [label2id_int[image] for image in examples["language"]]
     return inputs
-encoded_dataset_validation = dataset_validation.map(preprocess_function, remove_columns=['locale','client_id', 'path', 'audio', 'sentence', 'up_votes', 'down_votes', 'age', 'gender', 'accent','segment'], batched=True)
+encoded_dataset_validation = dataset_validation.map(preprocess_function, remove_columns=["id","num_samples", "path", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"], batched=True)
 from transformers import AutoModelForAudioClassification, TrainingArguments, Trainer
 
 model_name_extension = "".join(configs)
-model_name = model_checkpoint.split("/")[-1]+model_name_extension
+model_name = model_checkpoint.split("/")[-1]+model_name_extension+dataset_name
 
 args = TrainingArguments(
     f"{model_name}",#{model_name}arnlpt
@@ -67,7 +67,7 @@ def compute_metrics(eval_pred):
 
 
 best_model = AutoModelForAudioClassification.from_pretrained(
-    f"wav2vec2-baseukabplru_bestmodel"
+    f"wav2vec2-basefr_frde_denl_nlfleurs_fleurs_bestmodel"
 )
 
 trainer = Trainer(
@@ -76,13 +76,16 @@ trainer = Trainer(
     tokenizer=feature_extractor,
     compute_metrics=compute_metrics
 )
-
+# for batch in trainer.get_train_dataloader():
+#     break
+# from scipy.io.wavfile import write
+# write('output_sounddevice.wav', 16000, np.array(batch["input_values"][0]))
 # print(f"after loading model:{trainer.evaluate()}")
 pred= trainer.predict(encoded_dataset_validation)
 print(pred)
-inp = encoded_dataset_validation["input_values"]
-labels_p = encoded_dataset_validation["label"]
 best_model = best_model.wav2vec2
+inp = encoded_dataset_validation["input_values"][::2]
+labels_p = encoded_dataset_validation["labels"][::2]
 pred = best_model(torch.tensor(inp))
 pred = pred.last_hidden_state.reshape(pred.last_hidden_state.shape[0],-1)
 pred = pred.detach().numpy()
@@ -101,11 +104,11 @@ xdata = pred[:,0]
 ydata = pred[:,1]
  
 # Plot 3D plot
-ax.scatter(xdata, ydata,c=labels_p)
+scatter =ax.scatter(xdata, ydata,c=labels_p)
  
 # Plot title of graph
 plt.title(f'PCA')
-ax.legend()
+ax.legend(handles=scatter.legend_elements()[0],labels=labels)
 plt.show()
 
 print("end")
